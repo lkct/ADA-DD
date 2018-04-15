63322225f79eedadcda397d2cdc8c29baf68fb76
lkct <liangkct@yahoo.com>
2018-04-15 14:56:27 +0800

    modified trans

--- aug/trans.py
+++ aug/trans.py
@@ -10,16 +10,7 @@ args = parser.parse_args()
 
 img = open(args.file, 'rb+')
 
-_, len, _, _ = struct.unpack('>IIII', img.read(16))
-for i in range(len):
-    if i % 1000 == 0:
-        print i
-    img.seek(16 + i * 784)
-    s, = struct.unpack('784s', img.read(784))
-    s = [ord(c) for c in s]
-    s = np.array(s, dtype=np.uint8).reshape((28, 28))
-    s = np.transpose(s).reshape((-1, ))
-    s = [chr(c) for c in s]
-    s = "".join(s)
-    img.seek(-784, 1)
-    img.write(struct.pack('784s', s))
+img.seek(16)
+s = np.fromstring(img.read(), dtype=np.uint8).reshape((-1, 28, 28))
+img.seek(16)
+img.write(np.transpose(s, (0, 2, 1)).reshape((-1, )).tostring())
