9cd211b5eb410a555ea2ff2330bd1cc61cb0f390
PKU-62419 <37757257+PKU-62419@users.noreply.github.com>
2018-05-06 16:37:56 +0800

    Add files via upload
    a little polishment

--- aug/linen.py
+++ aug/linen.py
@@ -13,15 +13,14 @@ img = open(args.file, 'rb+')
 img.seek(16)
 s = np.fromstring(img.read(), dtype=np.uint8).reshape((-1, 28, 28))
 for i in range(s.shape[0]):
-    n = rd.randint(0, 3)
+    n = rd.randint(0, 2)
     if n == 1:
-        l = rd.randint(0, 27)
+        l = rd.randint(0, 26)
         for k in range(28):
             s[i][l][k] = s[i][l + 1][k] = 255
     elif n == 2:
-        ls = rd.randint(0, 13)
+        ls = rd.randint(0, 12)
         for k in range(28):
-            s[i][l][k] = s[i][l + 1][k] =
-            s[i][l + 14][k] = s[i][l + 15][k] = 255
+            s[i][ls][k] = s[i][ls + 1][k] = s[i][ls + 14][k] = s[i][ls + 15][k] = 255
 img.seek(16)
 img.write(np.transpose(s, (0, 2, 1)).reshape((-1, )).tostring())
