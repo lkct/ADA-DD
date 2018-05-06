d4fdc41bfc1d3063916dbb33725de0dcffeedfa4
PKU-62419 <37757257+PKU-62419@users.noreply.github.com>
2018-05-06 10:16:23 +0800

    Add files via upload
    new line added into project pictures

---
+++ aug/linen.py
@@ -0,0 +1,27 @@
+import argparse
+import struct
+import numpy as np
+import random as rd
+
+parser = argparse.ArgumentParser(description="transpose ubyte image")
+parser.add_argument('--file', type=str,
+                    default='emnist-digits-train-images-idx3-ubyte', help='ubyte file')
+args = parser.parse_args()
+
+img = open(args.file, 'rb+')
+
+img.seek(16)
+s = np.fromstring(img.read(), dtype=np.uint8).reshape((-1, 28, 28))
+for i in range(s.shape[0]):
+    n = rd.randint(0, 3)
+    if n == 1:
+        l = rd.randint(0, 27)
+        for k in range(28):
+            s[i][l][k] = s[i][l + 1][k] = 255
+    elif n == 2:
+        ls = rd.randint(0, 13)
+        for k in range(28):
+            s[i][l][k] = s[i][l + 1][k] =
+            s[i][l + 14][k] = s[i][l + 15][k] = 255
+img.seek(16)
+img.write(np.transpose(s, (0, 2, 1)).reshape((-1, )).tostring())
