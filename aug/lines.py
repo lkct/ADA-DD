91608699bd417ad6d9236ac8766ad8c905b57600
Jaelyn <Jaelyn@Jaelyns-MacBook-Pro.local>
2018-05-06 08:22:34 +0800

    lines

--- aug/lines.py
+++ aug/lines.py
@@ -1,23 +1,27 @@
 from random import *
 import argparse
-from PIL import Image, ImageDraw
+import numpy as np
+
 parser = argparse.ArgumentParser(description="add lines")
 parser.add_argument('--file', type=str,
                     default='train-images-idx3-ubyte', help='ubyte file')
 parser.add_argument('--no', type=int, default=0, help='image number in ubyte')
-parser.add_argument('--out', type=str, default='lines.bmp', help='output  image')
+parser.add_argument('--out', type=str, default='lines.bmp', help='output image')
 args = parser.parse_args()
 
-head = Image.open('5.bmp')
+head = open('5.bmp', 'rb')
+img = open(args.file, 'rb')
+out = open(args.out, 'wb')
 
+out.write(head.read(0x10))
+img.seek(16)
+a = np.fromstring(img.read(), dtype = np.uint8)
 
+b = a.reshape((60000, 28, 28))
 
-for i in range(randint(1,3)) :
-    draw = ImageDraw.Draw(head)
-    num = (randint (1,3))%3
+for i in range(2048,28):
+    b[i,:,0:1] = 0
     spaceing = uniform(1, 10)
     starting_point = uniform(0,14-spaceing)
-    draw.line((0, starting_point+spaceing*num, 28+starting_point+spaceing*num, starting_point+spaceing*num), fill=256,width=randint(2,3))
-    del draw
-
-head.show()
+    out.write(b.read(28))
+out.write(head.read(0x436))
