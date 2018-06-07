7b0812dcfd58dbf78eb161516da58c78e7592991
lkct <liangkct@yahoo.com>
2018-06-07 20:43:33 +0800

    v1.1

--- ddr/dev/col.py
+++ ddr/dev/col.py
@@ -4,6 +4,7 @@ from glob import glob
 filenames = glob("./*.jpg")
 for f in filenames:
     img = cv2.imread(f)
+    img = cv2.resize(img, None, fx=0.5, fy=0.5)
     img = 255 - img
     oimg = img
     _, img = cv2.threshold(img, 63, 0, cv2.THRESH_TOZERO)
@@ -17,13 +18,15 @@ for f in filenames:
     h, w, _ = img.shape
     for i in range(h):
         for j in range(w):
-            if img[i, j, 0] < 48:
+            if img[i, j, 0] < 80:
                 oimg[i, j] = 0
+    img[:, :, 0] = img[:, :, 2]
+    img[:, :, 1] = img[:, :, 2]
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    out = oimg[0:800, 0:800, :]
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    out = oimg[0:900, 0:1600, :]
     cv2.imshow(f, out)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    break
+    #break
